fc942a7e89238b72f13e5c1315772682febc5a83
Filipe Pires <liipepires@gmail.com>
2018-07-24 16:28:24 -0300

    [rmv] questao6

--- scripts/06 - View_frete_abril.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW VALORES_FRETES_ABRIL AS
-select 
- NOME as "Transportadora", sum(FRETE) as "Valor dos Fretes"
- from TRANSPORTADORA
- inner join VENDA
- on ID_TRANSP = FK_TRANSPORTADORA_ID_TRANSP
- where EXTRACT(MONTH from DT_ENTREGA) = 4
-group by NOME

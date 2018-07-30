830113f77fbf8139e17ac28059d854fceea8e7db
Filipe Pires <liipepires@gmail.com>
2018-07-30 03:39:30 -0300

    [MOD] VENDA_PRODUTO -> ITENS_VENDA

--- scripts/01 VIEW_DESC_F.sql
+++ scripts/01 VIEW_DESC_F.sql
@@ -1,6 +1,6 @@
 CREATE VIEW DESC_F AS
       SELECT c.NOME, c.ID_CLIENTE
-      FROM CLIENTE c, (SELECT vp.FK_VENDA_ID_VENDA, v.ID_VENDA, v.ID_CLIENTE
-                       FROM VENDA_PRODUTO vp, VENDA v
-                       WHERE vp.DESCONTO > 20 AND vp.FK_VENDA_ID_VENDA = v.ID_VENDA) x
+      FROM CLIENTE c, (SELECT vp.ID_VENDA, v.ID_VENDA, v.ID_CLIENTE
+                       FROM ITENS_VENDA vp, VENDA v
+                       WHERE vp.DESCONTO > 20 AND vp.ID_VENDA = v.ID_VENDA) x
       WHERE SEXO = 'F' AND x.ID_CLIENTE = c.ID_CLIENTE

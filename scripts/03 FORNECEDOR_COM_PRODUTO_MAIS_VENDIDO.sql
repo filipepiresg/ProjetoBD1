e371406b72ca62ddcb6226cc7f0bd93cbd0894b1
Andre Goncalves <andre335@gmail.com>
2018-07-30 19:36:20 -0300

    trigger 11 e 12 e correcao na consulta 3

--- scripts/03 FORNECEDOR_COM_PRODUTO_MAIS_VENDIDO.sql
+++ scripts/03 FORNECEDOR_COM_PRODUTO_MAIS_VENDIDO.sql
@@ -1,4 +1,4 @@
-/*33.	Mostre o(s) nome(s) do(s) fornecedor(s) do(s) produto(s) mais vendido(s) 
+/*3.	Mostre o(s) nome(s) do(s) fornecedor(s) do(s) produto(s) mais vendido(s) 
 (produtos que estão em mais vendas) */
 
 
@@ -7,7 +7,7 @@ FROM FORNECEDOR f, PRODUTO p, (SELECT v.ID_PROD
                                FROM ITENS_VENDA v
                                WHERE v.QUANTIDADE = (SELECT MAX(QUANTIDADE) 
                          FROM ITENS_VENDA )) iv
-WHERE p.ID_PROD = Iv.ID_PROD AND f.ID_FORN =  p.ID_FORN                     
+WHERE p.ID_PROD = iv.ID_PROD AND f.ID_FORN =  p.ID_FORN                     
                 
 
                

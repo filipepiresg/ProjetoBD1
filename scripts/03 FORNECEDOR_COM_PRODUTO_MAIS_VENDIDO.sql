36520cffdfb531f27b5c61d4505e3ca1c8218c16
LeandrodLima <leandrodsouto@gmail.com>
2018-07-29 21:09:00 -0300

    Questao 3

---
+++ scripts/03 FORNECEDOR_COM_PRODUTO_MAIS_VENDIDO.sql
@@ -0,0 +1,14 @@
+/*33.	Mostre o(s) nome(s) do(s) fornecedor(s) do(s) produto(s) mais vendido(s) 
+(produtos que estão em mais vendas) */
+
+
+SELECT f.NOME
+FROM FORNECEDOR f, PRODUTO p, (SELECT v.ID_PROD
+                               FROM ITENS_VENDA v
+                               WHERE v.QUANTIDADE = (SELECT MAX(QUANTIDADE) 
+                         FROM ITENS_VENDA )) iv
+WHERE p.ID_PROD = Iv.ID_PROD AND f.ID_FORN =  p.ID_FORN                     
+                
+
+               
+             
